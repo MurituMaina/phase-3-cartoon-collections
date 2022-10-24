c8c88889461f0937d04fc80060b743ae67ebdb0e
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-24 18:38:12 +0300

    complete tasks

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,48 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index  { |dwarf,index|
+  puts "#{index+1}. #{dwarf}"
+}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planteer_calls)
+  calls = [] 
+  planteer_calls.map {|planeteer| 
+  calls <<"#{planeteer.capitalize}!"
+    puts calls
+  }
+  puts calls
+    return calls
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(long_calls)
+  values = []
+
+  long_calls.each {|call| 
+  if call.length > 4
+     values << true
+  else
+ 
+    values << false
+  end
+}
+if values.include?(true)
+  return true
+else
+  return false
+end  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(food)
+cheese_types = ["cheddar", "gouda", "camembert"]
+food.find {
+  |cheese|
+  if cheese_types.include?(cheese)
+    puts cheese
+  true
+  else
+      false
+  end
+}
 end
